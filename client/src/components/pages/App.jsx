51ee20c521b776b403e6f46076d0c21f52af2f3b
Arijit <arijit123roy098@gmail.com>
2025-09-08 18:39:32 +0530

    server included

---
+++ client/src/components/pages/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Homepage from '../code/Homepage';
+import Signin from './Signin';
+import Signup from './Signup';
+import ProtectedRoute from './ProtecterRoute';
+import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import Content from '../code/Content';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Redirect root route based on Clerk auth */}
+        <Route path="/" element={<Homepage/>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <SignedIn>
+                <Navigate to="/content" />
+              </SignedIn>
+              <SignedOut>
+                <Navigate to="/signup" />
+              </SignedOut>
+            </>
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Signin />} />
+        <Route
+          path="/content"
+          element={
+            <ProtectedRoute>
+            <Content/> 
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
